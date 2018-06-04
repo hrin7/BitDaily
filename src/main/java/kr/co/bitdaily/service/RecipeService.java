8f12e91d15c4b974ed2bdeeee6e2e28da000d511
DESKTOP-S7DV3GR\khrin <khrin126@naver.com>
2018-06-04 17:26:20 +0900

    환경설정에 db 추가

--- src/main/java/kr/co/bitdaily/service/RecipeService.java
+++ src/main/java/kr/co/bitdaily/service/RecipeService.java
@@ -7,5 +7,5 @@ import org.springframework.web.bind.annotation.RequestParam;
 import kr.co.bitdaily.repository.vo.Recipe;
 
 public interface RecipeService {
-	List<Recipe> retrieveListBoard(@RequestParam(value="pageNo", defaultValue="1") int pageNo);
+	List<Recipe> retrieveListBoard(int pageNo);
 }
